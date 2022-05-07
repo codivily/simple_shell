dcc3ca407c2a364a32fc53089e15d07ab82608d4
codivily <codivily@gmail.com>
2022-05-07 06:15:38 -0700

    main.c file added

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: An integer
+ * @argv: List of command line arguments
+ *
+ * Return: An integer
+ */
+int main (int argc, char **argv)
+{
+	
+	return (0);
+}
