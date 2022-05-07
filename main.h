bb2781f95bfc9b1a7be5c85cd10274b03b69e3e2
Ngoamie <ngomeme28@gmail.com>
2022-05-07 06:33:32 -0700

    main.h file added

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/types.h>
+
+int _putchar(char c);
+void _puts(char *str);
