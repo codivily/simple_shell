97085e683675b9afc77f72fb71190cd43783b3b2
codivily <codivily@gmail.com>
2022-05-16 22:52:00 -0700

    PATH handling added

---
+++ _strdup.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+
+/**
+ *_strdup - duplicate a string
+ *@str: A string
+ *
+ *Return: A pointer to the duplicate string
+ */
+char *_strdup(const char *str)
+{
+	size_t i = 0;
+	size_t len = 0;
+	char *_str = NULL;
+
+	while (str[i++])
+	;
+	len += i;
+
+	_str = malloc(sizeof(*_str) * (len + 1));
+
+	i = 0;
+	while (i <= len)
+	{
+		_str[i] = str[i];
+		i++;
+	}
+
+	return (_str);
+}
--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include "main.h"
-#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
 
 /**
  * main - Entry point
@@ -8,28 +10,46 @@
  *
  * Return: An integer
  */
-int main(int argc, const char **argv)
+int main(int argc, char **argv)
 {
 	char *cmd_str = NULL;
 	char **args = NULL;
+	int exit_code = 0;
+	int status = 0;
 
 	/* an infinite loop */
 	while (1)
 	{
-		/* read the command from the user */
-		cmd_str = prompt("#cisfun$ ");
+		/* command line args support */
+		if (argc > 1)
+			args = argv + 1;
+		else
+		{
+			/* prompt the user  to enter command */
+			cmd_str = prompt("#cisfun$ ");
+			/* command args */
+			args = to_args(cmd_str);
+		}
 
-		/* command args */
-		args = to_args(cmd_str);
+		*args = locate(*args);
+		/* run the command in a child process */
+		if (*args)
+		{
+			if(fork() == 0)
+			{
+				exit_code = execve(*args, args, NULL);
+				dprintf(STDERR_FILENO, "%s: No such file or directory\n", cmd_str);
+				free(cmd_str);
+				exit(exit_code);
+			}
+			wait(&status);
+		}
+		else
+			dprintf(STDERR_FILENO, "%s: No such file or directory\n", cmd_str);
 
-		/* execute and check for error */
-		execve(args[0], args, NULL);
-
-		dprintf(STDERR_FILENO, "%s: No such file or directory\n", argv[0]);
-
-		/* release malloced memory from prompt function call */
 		free(cmd_str);
-
+		if (argc > 1)
+			exit(status);
 	}
 
 
