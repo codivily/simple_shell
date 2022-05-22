6e3a514b19c070211d8033047f37d6d579cd6d85
codivily <codivily@gmail.com>
2022-05-22 15:06:21 -0700

    update:

--- test/run.sh
+++ test/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DIR="$(dirname "$(realpath "$0")")"
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 $DIR/../*.c -o $DIR/shell && $DIR/shell
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89 $DIR/../*.c -o $DIR/shell && valgrind $DIR/shell
